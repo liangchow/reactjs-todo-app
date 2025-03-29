b8ef754a4e22baed1fbaa2684394eb012fee1611
liangchow <liangchern@gmail.com>
2025-03-30 00:12:11 +0800

    Update handleEdit

--- components/TodoCard.jsx
+++ components/TodoCard.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 export default function TodoCard(props) {
-    const { children, handleDeleteTodo, index  } = props
+    const { children, handleDeleteTodo, handleEditTodo, index  } = props
 
   return (
     <li className='todoItem'>
         {children}
         <div className='actionsContainer'>
             {/* Edit button */}
-            <button ><i className="fa-solid fa-pen-to-square"></i></button>
+            <button onClick={() => handleEditTodo(index)}><i className="fa-solid fa-pen-to-square"></i></button>
 
             {/* Delete button */}
             <button onClick={() => handleDeleteTodo(index)}><i className="fa-solid fa-trash"></i></button>
--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,37 @@ import TodoInput from '../components/TodoInput'
 import TodoList from '../components/TodoList'
 
 function App() {
-
+  // Initiaze a todos list
   const [todos, setTodos] = useState([
         "Eat chocolate with tomato juiced to fine-grained puree",
         "Go to gym",
       ])
+
+  // For inputting new todo items
   const [todoValue, setTodoValue] = useState('')
 
   function handleAddTodo(newTodo){
+    // Append newTodo item to the remaining todos list
     const newTodoList = [newTodo, ...todos]
     setTodos(newTodoList)
-  }
+  }    
 
   function handleDeleteTodo(index){
-    const updatedTodoList = todos.filter((todo, todoIndex) => todoIndex !== index)
+    const updatedTodoList = todos.filter((todo, todoIndex) => todoIndex != index)
     setTodos(updatedTodoList)
   }
-  
 
+  function handleEditTodo(index){
+    const todoValueToBeEdited = todos[index]
+    setTodoValue(todoValueToBeEdited)
+    handleDeleteTodo(index)
+  }
 
   return (
     <>
       <TodoInput handleAddTodo={handleAddTodo} todoValue={todoValue} setTodoValue={setTodoValue} />
-      <TodoList todos={todos} handleDeleteTodo={handleDeleteTodo} />
+      <TodoList todos={todos} handleDeleteTodo={handleDeleteTodo} handleEditTodo={handleEditTodo} />
     </>
-  )
-}
+  )}
 
 export default App
